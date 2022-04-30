2df4bbbf4a18889c406a599b0e08a8d27de50dae
GenomZ <kbuzar@gmail.com>
2022-04-30 12:42:47 +0200

    Debugging build_features stage1.

--- src/features/build_features_test.py
+++ src/features/build_features_test.py
@@ -1,103 +1,79 @@
 import pandas as pd
 import category_encoders as ce
-import requests
-import numpy as np
-import seaborn as sn
-import random
-import matplotlib.pyplot as plt
-from scipy import stats
-from scipy.stats import t, norm
-from statsmodels.graphics.gofplots import qqplot
-from statsmodels.stats import power
-from scipy.stats import shapiro
-import warnings
-import plotly
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from feature_engine.encoding import CountFrequencyEncoder
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
 import os
 
-dirname = os.path.dirname(__file__)
-df = pd.read_csv(os.path.join(dirname, r"./../../data/raw/test_values.csv"), decimal=",").reset_index(drop=True)
 
-# encoder = ce.binary.BinaryEncoder(cols=None, return_df=True)
-# data = encoder.fit_transform(test_v)
+def build_features_test_data(test_values_file_path, use_vanilla_data=False):
+    dirname = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(dirname, test_values_file_path), decimal=",").reset_index(drop=True)
 
-#mapping dictionary from build_features
-# data['geo_dam'] = data['geo_level_3_id'].map(dictionary_geo_dam)
-# data['geo_dam'] = data['geo_dam'].fillna(value=0)
-# print(data['geo_dam'])
-# df = data.copy()
-# converting geolocation 1 to use a frequency encoder
-#freq_data_1 = CountFrequencyEncoder(encoding_method="frequency", variables=["geo_level_1_id"])
-# fitting the encoder
-#freq_data_1.fit(data_encoded)
-# creates a dictionary of frequency to categories
-#dict_1 = freq_data_1.encoder_dict_
-# creating a new column filled using dictionary
-#data_encoded["geo_1"] = data_encoded["geo_level_1_id"].map(dict_1['geo_level_1_id'])
+    if use_vanilla_data:
+        # Dropping cathegorical from vanilla data
+        df = df.drop(['land_surface_condition'], axis=1)
+        df = df.drop(['foundation_type'], axis=1)
+        df = df.drop(['roof_type'], axis=1)
+        df = df.drop(['ground_floor_type'], axis=1)
+        df = df.drop(['other_floor_type'], axis=1)
+        df = df.drop(['position'], axis=1)
+        df = df.drop(['plan_configuration'], axis=1)
+        df = df.drop(['legal_ownership_status'], axis=1)
 
-# converting geolocation 2 to use a frequency encoder
-#freq_data_2 = CountFrequencyEncoder(encoding_method="frequency", variables=["geo_level_2_id"])
-# fitting the encoder
-#freq_data_2.fit(data_encoded)
-# creates a dictionary of frequency to categories
-#dict_2 = freq_data_2.encoder_dict_
-# creating a new column filled using dictionary
-#data_encoded["geo_2"] = data_encoded["geo_level_2_id"].map(dict_2['geo_level_2_id'])
+        # Dropping poorly correlated columns
+        df = df.drop(['count_families'], axis=1)
+        df = df.drop(['has_secondary_use'], axis=1)
+        df = df.drop(['has_secondary_use_agriculture'], axis=1)
+        df = df.drop(['has_secondary_use_hotel'], axis=1)
+        df = df.drop(['has_secondary_use_rental'], axis=1)
+        df = df.drop(['has_secondary_use_institution'], axis=1)
+        df = df.drop(['has_secondary_use_school'], axis=1)
+        df = df.drop(['has_secondary_use_industry'], axis=1)
+        df = df.drop(['has_secondary_use_health_post'], axis=1)
+        df = df.drop(['has_secondary_use_gov_office'], axis=1)
+        df = df.drop(['has_secondary_use_use_police'], axis=1)
+        df = df.drop(['has_secondary_use_other'], axis=1)
+        test_df = df
+        return test_df
 
-# converting geolocation 3 to use a frequency encoder
-#freq_data_3 = CountFrequencyEncoder(encoding_method="frequency", variables=["geo_level_3_id"])
-# fitting the encoder
-#freq_data_3.fit(data_encoded)
-# creates a dictionary of frequency to categories
-#dict_3 = freq_data_3.encoder_dict_
-# creating a new column filled using dictionary
-#data_encoded["geo_3"] = data_encoded["geo_level_3_id"].map(dict_3['geo_level_3_id'])
+    dirname = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(dirname, r"./../../data/raw/test_values.csv"), decimal=",").reset_index(drop=True)
 
-# Dropping redundant geolocations
-#df = data_encoded.drop(["geo_level_1_id", "geo_level_2_id", "geo_level_3_id"], axis=1)
+    # Normalising continuous variables
+    df['height_p_norm'] = (df['height_percentage'] - df['height_percentage'].min()) / (
+                df['height_percentage'].max() - df['height_percentage'].min())
+    df['area_p_norm'] = (df['area_percentage'] - df['area_percentage'].min()) / (
+                df['area_percentage'].max() - df['area_percentage'].min())
+    df['age_norm'] = (df['age'] - df['age'].min()) / (df['age'].max() - df['age'].min())
 
-# Normalising height_percentage, area_percentage, age
-# df['height_p_norm'] = (df['height_percentage'] - df['height_percentage'].min()) / (
-#         df['height_percentage'].max() - df['height_percentage'].min())
-# df['area_p_norm'] = (df['area_percentage'] - df['area_percentage'].min()) / (
-#         df['area_percentage'].max() - df['area_percentage'].min())
-# df['age_norm'] = (df['age'] - df['age'].min()) / (df['age'].max() - df['age'].min())
+    # dropping redundant variables
+    df.drop(
+        ['height_percentage', 'area_percentage', 'age', 'geo_level_2_id', 'geo_level_3_id', 'has_secondary_use_agriculture',
+         'has_secondary_use_institution', 'has_secondary_use_school', 'has_secondary_use_industry',
+         'has_secondary_use_school', 'has_secondary_use_health_post', 'has_secondary_use_gov_office',
+         'has_secondary_use_use_police', 'has_secondary_use_other'], axis=1, inplace=True)
 
-# Dropping redundant columns
-# test_df = df.drop(["height_percentage", "area_percentage", "age", "geo_level_2_id", "geo_level_3_id", "geo_level_1_id"],axis=1)
+    # Converting features to str to get encoder to work
+    df['count_floors_pre_eq'] = df['count_floors_pre_eq'].astype(str)
+    df['geo_level_1_id'] = df['geo_level_1_id'].astype(str)
+    df['count_families'] = df['count_families'].astype(str)
 
-# remaining_nan = test_df[test_df.isnull().any(axis=1)]
-# print(remaining_nan)
-#
-# file_name = 'TestDataSPAM29041745.csv'
-# df.to_csv(file_name, sep=',')
+    # set up the encoder
+    encoder = CountFrequencyEncoder(encoding_method='frequency',
+                                    variables=['geo_level_1_id', 'count_floors_pre_eq', 'land_surface_condition',
+                                               'foundation_type', 'roof_type', 'ground_floor_type', 'other_floor_type',
+                                               'position', 'plan_configuration', 'legal_ownership_status',
+                                               'count_families'])
+    # fit the encoder
+    encoder.fit(df)
+    df = encoder.transform(df)
 
-############################################################################################
-#Normalising continuous variables
-df['height_p_norm']=(df['height_percentage']-df['height_percentage'].min())/(df['height_percentage'].max()-df['height_percentage'].min())
-df['area_p_norm']=(df['area_percentage']-df['area_percentage'].min())/(df['area_percentage'].max()-df['area_percentage'].min())
-df['age_norm']=(df['age']-df['age'].min())/(df['age'].max()-df['age'].min())
+    # Adding binary modifiers
+    binary = ce.binary.BinaryEncoder(
+        cols=['has_superstructure_adobe_mud', 'has_superstructure_mud_mortar_stone', 'has_superstructure_stone_flag',
+              'has_superstructure_cement_mortar_stone', 'has_superstructure_mud_mortar_brick',
+              'has_superstructure_cement_mortar_brick', 'has_superstructure_timber', 'has_superstructure_bamboo',
+              'has_superstructure_rc_non_engineered', 'has_superstructure_rc_engineered', 'has_superstructure_other',
+              'has_secondary_use', 'has_secondary_use_hotel', 'has_secondary_use_rental'], return_df=True)
+    test_df = binary.fit_transform(df)
 
-#dropping redundant variables
-df.drop(['height_percentage','area_percentage','age', 'geo_level_2_id','geo_level_3_id','has_secondary_use_agriculture','has_secondary_use_institution', 'has_secondary_use_school', 'has_secondary_use_industry','has_secondary_use_school', 'has_secondary_use_health_post', 'has_secondary_use_gov_office','has_secondary_use_use_police', 'has_secondary_use_other'], axis=1, inplace=True)
-
-#Converting features to str to get encoder to work
-df['count_floors_pre_eq'] = df['count_floors_pre_eq'].astype(str)
-df['geo_level_1_id'] = df['geo_level_1_id'].astype(str)
-df['count_families'] = df['count_families'].astype(str)
-
-# set up the encoder
-encoder = CountFrequencyEncoder(encoding_method='frequency', variables=['geo_level_1_id', 'count_floors_pre_eq','land_surface_condition','foundation_type', 'roof_type','ground_floor_type','other_floor_type','position','plan_configuration', 'legal_ownership_status', 'count_families'])
-# fit the encoder
-encoder.fit(df)
-df = encoder.transform(df)
-
-#Adding binary modifiers
-binary = ce.binary.BinaryEncoder(cols=['has_superstructure_adobe_mud', 'has_superstructure_mud_mortar_stone','has_superstructure_stone_flag','has_superstructure_cement_mortar_stone', 'has_superstructure_mud_mortar_brick', 'has_superstructure_cement_mortar_brick', 'has_superstructure_timber', 'has_superstructure_bamboo', 'has_superstructure_rc_non_engineered', 'has_superstructure_rc_engineered', 'has_superstructure_other', 'has_secondary_use','has_secondary_use_hotel','has_secondary_use_rental'], return_df=True)
-df = binary.fit_transform(df)
-print(df.head())
+    return test_df
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from src.data.make_dataset import read_train_data
+from src.visualization.visualize import visualise_train_data
 from src.features.build_features import build_features
 from src.models.train_model import train_models_with_random_search_cv, train_models_with_grid_search_cv
 from src.models.train_model_noGridCV import train_no_grid_cv
-from src.data.make_test_data import create_test_data
+from src.features.build_features_test import build_features_test_data
 from src.data.split_data import split_train_dataset
 from src.models.predict_model import evaluate_all_models
 from src.models.test_models import test_models_generate_results
@@ -14,6 +15,14 @@ Read training data labels and values. Creating training dataset.
 """
 df = read_train_data(r'../../data/raw/train_labels.csv', r'../../data/raw/train_values.csv')
 
+
+"""
+Visualise the training data to study features.
+Specific to Richter's Predictor: Modeling Earthquake Damage
+https://www.drivendata.org/competitions/57/nepal-earthquake
+"""
+visualise_train_data(df)
+
 """
 Build features for training set.
 If dropping all categorical and low corr data use: use_vanilla_data=True (default: False)
@@ -22,8 +31,12 @@ To use this routine on a different data set, You have to edit build_features and
 value_column_string in split_train_dataset as
 this is specific to Richter's Predictor: Modeling Earthquake Damage
 https://www.drivendata.org/competitions/57/nepal-earthquake
+
+use_vanilla_data is made to a global variable since it needs to be consistent 
+for train and test datasets
 """
-train_df = build_features(df, use_vanilla_data=True)
+use_vanilla_data = False
+train_df = build_features(df, use_vanilla_data=use_vanilla_data)
 
 """
 Split the train dataframe according to value_column_string and train_test_split params
@@ -70,7 +83,7 @@ Additionally a cross validation score will be generated for the train dataset
 """
 Create the test dataset to generate results for upload
 """
-test_df = create_test_data(r"./../../data/raw/test_values.csv", use_vanilla_data=True)
+test_df = build_features_test_data(r"./../../data/raw/test_values.csv", use_vanilla_data=use_vanilla_data)
 
 """
 Apply test dataset to all trained models ang generate results.
